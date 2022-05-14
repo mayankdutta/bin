9db177d95bcbf6a858a050b6cb7fa0635c9f3cd7
mayank dutta <smayankdutt@gmail.com>
2022-05-14 08:50:48 +0530

    script updated

--- run.sh
+++ run.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-echo -n "Enter fileName : "
-read PROG_NAME
-PROG_NAME="$PROG_NAME.cpp"
+echo -n "STARTED" # run info.cpp in out
+PROG_NAME=$1
 
-output=$(g++ --std=c++17 -Wall -Wextra $PROG_NAME 2>&1)
+# echo "total length of arguments is $#"
+# echo "$1"
+# echo "$2"
+# echo "$3"
+# echo "$4"
+
+output=$(g++ --std=c++20 -Wall -Wextra -Wshadow -fsanitize=undefined -fsanitize=address $PROG_NAME 2>&1)
 if [[ $? != 0 ]]; then
 	# There was an error, display the error in $output
 	echo -e "Error:\n$output"
 else
 	# Compilation successfull
-	echo -n "Input file, press ENTER if not have any : "
-	read input
-
-	if
-		[[ -z "$input" ]]
-	then
-		./a.out
-	else
-		./a.out <$input
-	fi
+  if (($# == 1)); then
+    ./a.out
+  elif (($# == 2)); then 
+    ./a.out < $2
+  elif (($# == 3)); then 
+    ./a.out < $2 > $3
+  fi
 fi
 
 # source: https://stackoverflow.com/questions/59768931/bash-script-to-compile-and-run-c-program
+
+
+
