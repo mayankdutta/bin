06b162bb5cbe565acfab88c51175f4f44730e08f
mayank dutta <smayankdutt@gmail.com>
2022-06-25 11:33:21 +0530

    updated

--- run.sh
+++ run.sh
@@ -9,22 +9,19 @@ PROG_NAME=$1
 # echo "$3"
 # echo "$4"
 
-output=$(g++ --std=c++20 -Wall -Wextra -Wshadow -fsanitize=undefined -fsanitize=address $PROG_NAME 2>&1)
+output=$(g++ --std=c++20 -Wall -Wextra -Wformat=2 -Wfloat-equal -Wshift-overflow=2 -Wduplicated-cond -Wshadow -Wconversion -fsanitize=undefined -fsanitize=address $PROG_NAME 2>&1)
 if [[ $? != 0 ]]; then
 	# There was an error, display the error in $output
 	echo -e "Error:\n$output"
 else
 	# Compilation successfull
-  if (($# == 1)); then
-    ./a.out
-  elif (($# == 2)); then 
-    ./a.out < $2
-  elif (($# == 3)); then 
-    ./a.out < $2 > $3
-  fi
+	if (($# == 1)); then
+		./a.out
+	elif (($# == 2)); then
+		./a.out <$2
+	elif (($# == 3)); then
+		./a.out <$2 >$3
+	fi
 fi
 
 # source: https://stackoverflow.com/questions/59768931/bash-script-to-compile-and-run-c-program
-
-
-
